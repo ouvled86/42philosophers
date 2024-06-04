1cea4dc0d5241046b88fb775f10f4e5ad7e76032
Oualid El Boukhari <ouel-bou@c1r8p5.1337.ma>
2024-06-04 16:56:32 +0100

    Added necessary libraries to header file

---
+++ philo/includes/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ouel-bou <ouel-bou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 11:33:06 by ouel-bou          #+#    #+#             */
+/*   Updated: 2024/06/04 12:20:45 by ouel-bou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+
+
+#endif
